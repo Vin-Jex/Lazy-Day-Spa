df89185030a0b4ff6d39ea7b0ed5bba9f9f95a05
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-09-27 08:42:46 +0100

    Centralize isFetching,  isLoading handler and setting up a default isError handler

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -13,7 +12,8 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
-  const { data } = useQuery(queryKeys.treatments, getTreatments);
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
 }
